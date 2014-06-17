72b461adabb41118a47cc0cc99e9d743834f223c
Andreas Wurzer <andreas.wurzer@novum-ccservices.at>
2014-06-17 13:50:12 +0200

    fix LICENCE, .gitignore, add initial source

---
+++ project.clj
@@ -0,0 +1,11 @@
+(defproject enclojean "0.1.0-SNAPSHOT"
+  :description "Clojure EnOcean Library"
+  :url "http://github.com/andeee/enclojean"
+  :license {:name "Apache Licence, Version 2.0"
+            :url "http://www.apache.org/licenses/LICENSE-2.0"}
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [aleph "0.3.2"]]
+  :profiles {:dev
+             {:dependencies [[midje "1.6.3"]]
+              :plugins [[lein-midje "3.1.1"]]}}
+  :plugins [[cider/cider-nrepl "0.7.0-SNAPSHOT"]])
