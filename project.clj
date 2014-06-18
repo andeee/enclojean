9ade099c1e8c9c0cd4d64006e55de8864de111e9
Andreas Wurzer <andeee@andeee.at>
2014-06-18 17:02:31 +0200

    extract crc8 implementation to type namespace, add test vectors, add licence to README
    test vectors are extracted from http://blog.xivo.io/public/smbus_pec_crc8_test_vectors.txt

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "Apache Licence, Version 2.0"
             :url "http://www.apache.org/licenses/LICENSE-2.0"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [aleph "0.3.2"]]
+                 [gloss "0.2.2"]]
   :profiles {:dev
              {:dependencies [[midje "1.6.3"]]
               :plugins [[lein-midje "3.1.1"]]}}
--- test/enclojean/core_test.clj
+++ test/enclojean/core_test.clj
@@ -1,8 +1,3 @@
 (ns enclojean.core-test
   (:use midje.sweet)
   (:require [enclojean.core :as core]))
-
-(facts "about `calc-crc8`"
-  (fact "crc of empty or 0 is 0"
-    (core/calc-crc8 (byte-array [(byte 0)])) => 0
-    (core/calc-crc8 (byte-array 0)) => 0))
