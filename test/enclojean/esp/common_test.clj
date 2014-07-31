e18b45da5034895c0b3751b1b2de396d1b45d41f
Andreas Wurzer <andeee@andeee.at>
2014-07-31 16:01:52 +0200

    add more common commands

--- test/enclojean/esp/common_test.clj
+++ test/enclojean/esp/common_test.clj
@@ -6,44 +6,47 @@
             [gloss.io :refer [decode encode]]
             [byte-streams :refer [to-byte-buffers to-byte-array]]))
 
+(defn unchecked-byte-vec [v]
+  (vec (bytes/from-seq v)))
+
 (defn encode-esp [decoded]
   (-> (encode esp decoded) to-byte-array bytes/to-seq))
 
 (defn decode-esp [telegram]
-  (decode esp (to-byte-buffers telegram)))
+  (decode esp (to-byte-buffers (bytes/from-seq telegram))))
 
-(def read-version-telegram (bytes/from-seq 
-                            [0x55 0x00 0x01 0x00
-                             0x05 0x70 0x03 0x09]))
+(defn command [command-kw & params]
+  [:sync
+   (let [body [:packet-type :common-command :command command-kw]]
+     (apply array-map
+            (if (seq params)
+              (concat body params)
+              body)))])
 
-(def read-version-decoded [:sync {:packet-type :common-command 
-                                  :command :read-version}])
-
-(def write-reset-telegram (bytes/from-seq 
-                            [0x55 0x00 0x01 0x00
-                             0x05 0x70 0x02 0x0E]))
-
-(def write-reset-decoded [:sync {:packet-type :common-command 
-                                  :command :write-reset}])
-
-(def write-sleep-telegram (bytes/from-seq 
-                            [0x55 0x00 0x05 0x00
-                             0x05 0xDB 0x01 0x00 
-                             0x00 0x00 0x01 0x65]))
-
-(def write-sleep-decoded [:sync {:packet-type :common-command 
-                                 :command :write-sleep :sleep-period 1}])
-
-(facts "about common commands"
-  (fact "encodes read-version"
-    (encode-esp read-version-decoded) => (vec read-version-telegram))
-  (fact "decodes read-version"
-    (decode-esp read-version-telegram) => read-version-decoded)
-  (fact "encodes write-reset"
-    (encode-esp write-reset-decoded) => (vec write-reset-telegram))
-  (fact "decodes write-reset"
-    (decode-esp write-reset-telegram) => write-reset-decoded)
-  (fact "encodes write-sleep"
-    (encode-esp write-sleep-decoded) => (vec write-sleep-telegram))
-  (fact "decodes write-sleep"
-    (decode-esp write-sleep-telegram) => write-sleep-decoded))
+(tabular
+ (facts "about common commands"
+   (fact "encode works"
+     (encode-esp ?decoded) => (unchecked-byte-vec ?encoded))
+   (fact "decode works"
+     (decode-esp ?encoded) => ?decoded))
+ ?decoded                            ?encoded
+ (command :write-sleep
+          :sleep-period 1)           [0x55 0x00 0x05 0x00
+                                      0x05 0xDB 0x01 0x00 
+                                      0x00 0x00 0x01 0x65]
+ (command :read-version)             [0x55 0x00 0x01 0x00  
+                                      0x05 0x70 0x03 0x09]
+ (command :write-reset)              [0x55 0x00 0x01 0x00
+                                      0x05 0x70 0x02 0x0E]
+ (command :read-sys-log)             [0x55 0x00 0x01 0x00
+                                      0x05 0x70 0x04 0x1C]
+ (command :write-sys-log)            [0x55 0x00 0x01 0x00
+                                      0x05 0x70 0x05 0x1B]
+ (command :write-built-in-self-test) [0x55 0x00 0x01 0x00
+                                      0x05 0x70 0x06 0x12]
+ (command :write-id-base
+          :base-id 0xFF800000)       [0x55 0x00 0x05 0x00
+                                      0x05 0xDB 0x07 0xFF
+                                      0x80 0x00 0x00 0xF3]
+ (command :read-id-base)             [0x55 0x00 0x01 0x00
+                                      0x05 0x70 0x08 0x38])
