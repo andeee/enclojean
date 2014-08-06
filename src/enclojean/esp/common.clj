a6f123d4a0c75624418cdde6e716bd5ad96ede77
Andreas Wurzer <andreas.wurzer@novum-ccservices.at>
2014-08-06 21:54:59 +0200

    finish command definition, start impl of optional command paras

--- src/enclojean/esp/common.clj
+++ src/enclojean/esp/common.clj
@@ -16,6 +16,21 @@
 (def filter-kind-frame
   (enum :ubyte {:blocks 0x00, :apply 0x80}))
 
+(def and-or-frame
+  (enum :byte {:or 0, :and 1}))
+
+(def memory-type-frame
+  (enum :byte {:flash 0x00, :ram-0 0x01, :data-ram 0x02, :idata-ram 0x03, :xdata-ram 0x04}))
+
+(def memory-area-frame 
+  (enum :byte {:config-area 0, :smart-ack-table 1, :system-error-log 2}))
+
+(def direction-frame
+  (enum :byte {:inbound-table 0x00, :outbound-table 0x01, :outbound-table-broadcast 0x02}) )
+
+(def mode-frame
+  (enum :byte {:compatible-mode 0x00, :advanced-mode 0x01}))
+
 (def commands
   {:write-sleep [0x01 :sleep-period :uint32] 
    :write-reset 0x02
@@ -28,29 +43,29 @@
    :write-repeater-level [0x09 :repeater-enable on-off-frame :repeater-level repeater-level-frame]
    :read-repeater-level 0x0A
    :write-filter-add [0x0B :filter-type filter-type-frame :filter-value :uint32 :filter-kind filter-kind-frame]
-   :write-filter-delete 0x0C
+   :write-filter-delete [0x0C :filter-type filter-type-frame :filter-kind :uint32]
    :write-filter-delete-all 0x0D
-   :write-filter-enable 0x0E
+   :write-filter-enable [0x0E :filter-enable on-off-frame :filter-operator and-or-frame]
    :read-filters 0x0F
-   :write-wait-maturity 0x10
-   :write-subtelegram 0x11
-   :write-memory 0x12
-   :read-memory 0x13
-   :read-memory-address 0x14
+   :write-wait-maturity [0x10 :wait-end-maturity-enable on-off-frame]
+   :write-subtelegram [0x11 :write-subtelegram-enable on-off-frame]
+   :write-memory [0x12 :memory-type memory-type-frame :memory-address :uint32 :memory-data :ubyte]
+   :read-memory [0x13 :memory-type memory-type-frame :memory-address :uint32 :data-length :uint16]
+   :read-memory-address [0x14 :memory-area memory-area-frame]
    :read-security 0x15
-   :write-security 0x16
-   :write-learnmode 0x17
+   :write-security [0x16 :security-level :ubyte :key :uint32 :rolling-code :uint32]
+   :write-learnmode [0x17 :learn-mode-enable on-off-frame :timeout :uint32 :optional [:channel :ubyte]]
    :read-learnmode 0x18
-   :write-secure-device-add 0x19
-   :write-secure-device-delete 0x1A
-   :read-secure-device-by-index 0x1B
-   :write-mode 0x1C
-   :read-number-of-secure-devices 0x1D
-   :read-secure-device-by-id 0x1E
-   :write-secure-device-add-psk 0x1F
-   :write-secure-device-send-teachin 0x20
-   :write-temporary-rolling-code-window 0x21
-   :read-secure-device-psk 0x22})
+   :write-secure-device-add [0x19 :security-level-format :ubyte :device-id :uint32 :private-key (repeat 16 :ubyte) :rolling-code :uint24 :optional [:direction direction-frame]]
+   :write-secure-device-delete [0x1A :device-id :uint32 :optional [:direction direction-frame]]
+   :read-secure-device-by-index [0x1B :index :ubyte :optional [:direction direction-frame]]
+   :write-mode [0x1C :mode mode-frame]
+   :read-number-of-secure-devices [0x1D :optional [:direction direction-frame]]
+   :read-secure-device-by-id [0x1E :device-id :uint32 :optional [:direction direction-frame]]
+   :write-secure-device-add-psk [0x1F :device-id :uint32 :pre-shared-key (repeat 16 :ubyte)]
+   :write-secure-device-send-teachin [0x20 :device-id :uint32 :optional [:teach-in-info :ubyte]]
+   :write-temporary-rolling-code-window [0x21 :rolling-code-enable on-off-frame :rolling-code-window :uint32]
+   :read-secure-device-psk [0x22 :device-id :uint32]})
 
 (defn get-command-code [command] 
   (let [command-params (second command)]
@@ -64,13 +79,36 @@
 (def command-code-frame
   (enum :ubyte command-codes))
 
-(defn get-command-codec [command-kw]
-  (let [command-params (command-kw commands)]
-    (compile-frame
-     (apply ordered-map 
+(defn get-all-command-map [command-kw]
+  (let [command-params (command-kw commands)]  
+    (apply array-map
             (concat [:command command-kw]
                     (when (vector? command-params)
-                      (rest command-params)))))))
+                      (rest command-params))))))
+
+(defn get-command-map [command-kw]
+  (-> (get-all-command-map command-kw)
+      (dissoc :optional)))
+
+(defn get-optional-map [command-kw]
+  (apply array-map
+         (:optional (get-all-command-map command-kw))))
+
+(defn pre-to-codec [map-fn command-kw]
+  (-> (map-fn command-kw)
+      seq
+      flatten))
+
+(defn to-codec [map-fn command-kw]
+  (compile-frame
+   (apply ordered-map
+          (pre-to-codec map-fn command-kw))))
+
+(def get-command-codec
+  (partial to-codec get-command-map))
+
+(def get-optional-codec
+  (partial to-codec get-optional-map))
 
 (def common-command-frame
   (header command-code-frame
@@ -82,4 +120,52 @@
     (header->body [_ h] common-command-frame)
     (body->header [_ b] {:data-length (+ (sizeof command-code-frame)
                                          (sizeof (get-command-codec (:command b))))
-                         :optional-length 0})))
+                         :optional-length (sizeof (get-optional-codec (:command b)))})))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
