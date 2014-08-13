801e2802e8c4e8e59e23717b79c4d54b6ef4be36
Andreas Wurzer <andeee@andeee.at>
2014-08-13 20:55:47 +0200

    finished tests

--- project.clj
+++ project.clj
@@ -3,10 +3,12 @@
   :url "http://github.com/andeee/enclojean"
   :license {:name "Apache Licence, Version 2.0"
             :url "http://www.apache.org/licenses/LICENSE-2.0"}
-  :dependencies [[org.clojure/clojure "1.5.1"]
+  :dependencies [[org.clojure/clojure "1.6.0"]
                  [gloss "0.2.2"]
+                 [aleph "0.3.3"]
+                 [lamina "0.5.4"]
                  [byte-streams "0.1.11"]]
   :profiles {:dev
              {:dependencies [[midje "1.6.3"]]
               :plugins [[lein-midje "3.1.1"]]}}
-  :plugins [[cider/cider-nrepl "0.7.0-SNAPSHOT"]])
+  :plugins [[cider/cider-nrepl "0.8.0-SNAPSHOT"]])
