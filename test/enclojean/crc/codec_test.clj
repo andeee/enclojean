a230664754b8f7607b111e6bd384e5bf39b5f894
Andreas Wurzer <andeee@andeee.at>
2015-06-24 19:45:21 +0200

    cleanup tests

--- test/enclojean/crc/codec_test.clj
+++ test/enclojean/crc/codec_test.clj
@@ -2,22 +2,11 @@
   (:use midje.sweet)
   (:require [enclojean.crc.codec :refer [crc8-frame]]
             [enclojean.bytes :as bytes]
+            [enclojean.test-utils :refer [unchecked-byte-vec encode-crc8 decode-crc8]]
             [gloss.io :refer [encode decode]]
             [gloss.core :refer [compile-frame string header]]
             [gloss.core.codecs :refer [identity-codec]]
             [byte-streams :refer [to-byte-array]]))
-
-(defn encode-crc8 [codec v]
-  (-> (compile-frame codec)
-      crc8-frame
-      (encode v)
-      to-byte-array
-      bytes/to-seq))
-
-(defn decode-crc8 [codec x]
-  (-> (compile-frame codec)
-      crc8-frame
-      (decode x)))
 
 (facts "about `crc8-frame`"
   (tabular
@@ -29,8 +18,8 @@
    0x03    [0x03 0x09]
    0x04    [0x04 0x1C])
   (fact "decoding checks for crc8 checksum"
-    (decode-crc8 :byte (bytes/from-seq [0x01 0x07])) => 0x01
-    (decode-crc8 :byte (bytes/from-seq [0x01 0xFF])) => (throws Exception))
+    (decode-crc8 :byte [0x01 0x07]) => 0x01
+    (decode-crc8 :byte [0x01 0xFF]) => (throws Exception))
   (tabular
    (fact "nested header codec is correctly en/decoded"
      (let [b->h (fn [body]
@@ -43,17 +32,19 @@
                     2 (compile-frame [:b :float32])
                     3 (compile-frame [:c (string :utf-8 :delimiters [\0])])))
            codec (header :byte h->b b->h)]
-       (encode-crc8 codec ?decoded) => (bytes/unchecked-seq ?encoded)
-       (decode-crc8 codec (bytes/from-seq ?encoded)) => ?decoded))
+       (encode-crc8 codec ?decoded) => (unchecked-byte-vec ?encoded)
+       (decode-crc8 codec ?encoded) => ?decoded))
    ?decoded   ?encoded
    [:a 1]     [0x01 0x00 0x01 0x6C]
    [:b 2.5]   [0x02 0x40 0x20 0x00 0x00 0x1C]
    [:c "abc"] [0x03 0x61 0x62 0x63 0x30 0xAC])
   (tabular
    (fact "nested identity codec is correctly en/decoded"
-     (let [unpack (comp bytes/to-seq to-byte-array)]
-       (unpack (decode-crc8 identity-codec (bytes/from-seq ?encoded)))) => ?decoded
-       (encode-crc8 identity-codec (bytes/from-seq ?decoded)) => (bytes/unchecked-seq ?encoded))
+     (let [unpack (comp bytes/to-seq to-byte-array)
+           decode (partial decode-crc8 identity-codec)
+           encode (partial encode-crc8 identity-codec)]
+       (unpack (decode (bytes/from-seq ?encoded))) => ?decoded
+       (encode (bytes/from-seq ?decoded)) => (unchecked-byte-vec ?encoded)))
    ?decoded                   ?encoded
    [0x01]                     [0x01 0x07]
    [0x02]                     [0x02 0x0E]
--- test/enclojean/test_utils.clj
+++ test/enclojean/test_utils.clj
@@ -1,7 +1,9 @@
 (ns enclojean.test-utils
   (:require [enclojean.bytes :as bytes] 
             [enclojean.esp.core :refer [esp]]
+            [enclojean.crc.codec :refer [crc8-frame]]
             [byte-streams :refer [to-byte-buffers to-byte-array]]
+            [gloss.core :refer [compile-frame]]
             [gloss.io :refer [decode encode]]))
 
 (def unchecked-byte-vec (comp vec bytes/from-seq))
@@ -15,3 +17,14 @@
 
 (defn decode-esp [telegram]
   (decode esp (unchecked-byte-buffers telegram)))
+
+(defn encode-crc8 [codec v]
+  (-> (compile-frame codec)
+      crc8-frame
+      (encode v)
+      to-byte-seq))
+
+(defn decode-crc8 [codec x]
+  (-> (compile-frame codec)
+      crc8-frame
+      (decode (unchecked-byte-buffers x))))
